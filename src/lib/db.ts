a7649588e1b7734b6ec14d234cc64965268f8fdb
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2026-02-25 13:27:57 +0530

    first commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,12 @@
-// src/lib/db.ts
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
+import { PrismaClient } from '@prisma/client'
 
-const connectionString = process.env.DATABASE_URL;
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient()
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production')
+  globalForPrisma.prisma = prisma
