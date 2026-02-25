151d7cf90fb53a58f211b53130794d4453cf357b
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2026-02-25 17:59:28 +0530

    Basic file changes and remove build error

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,8 +7,6 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 export default defineConfig({
   migrations: {
-    // This tells Prisma how to run your seed file
-    seed: 'node --loader ts-node/esm ./prisma/seed.ts',
   },
   datasource: {
     url: process.env.DATABASE_URL,
